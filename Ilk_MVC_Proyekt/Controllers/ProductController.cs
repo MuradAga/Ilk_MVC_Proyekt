2db9a812a2f6d6dc34122a498ff5176a5ffbde6f
◯ 𝓜ｕｒａｄ ✦ <muradagazade1661@gmail.com>
2023-04-09 11:02:47 +0400

    Add project files.

---
+++ Ilk_MVC_Proyekt/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Ilk_MVC_Proyekt.Context;
+using Ilk_MVC_Proyekt.Entites;
+using Ilk_MVC_Proyekt.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ilk_MVC_Proyekt.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductContext _context;
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Ilk_MVC_Proyekt/Program.cs
@@ -0,0 +1,41 @@
+using Ilk_MVC_Proyekt.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ilk_MVC_Proyekt
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ProductContext>(item => item.UseSqlServer("Server=Murad; Database=ProductDb; Trusted_Connection=True; TrustServerCertificate=True;"));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
